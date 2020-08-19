78a854767360aa3372dfbf7ec74b10557e20f293
PavelStrykhar <spavlikv@gmail.com>
2020-08-19 23:57:12 +0300

    заготовки для проекта

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+
+musicPlayerInit();
+videoPlayerInit();
+radioPlayerInit();
